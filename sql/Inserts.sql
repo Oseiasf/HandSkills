1ab10e767d5c3ea1129bd7131314614e2adf3c47
Oseias <oseiasf25@gmail.com>
2018-02-23 16:24:40 -0300

    inserts no BD

--- sql/Inserts.sql
+++ sql/Inserts.sql
@@ -3,3 +3,4 @@ insert into MaterialDoProduto (id,descricao) values (2, "Argila");
 insert into MaterialDoProduto (id,descricao) values (3, "Pano");
 insert into MaterialDoProduto (id,descricao) values (4, "Papelão");
 insert into MaterialDoProduto (id,descricao) values (5, "Gesso");
+insert into MaterialDoProduto (id,descricao) values (6, "PET");
